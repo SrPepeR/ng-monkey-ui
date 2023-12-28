be1d37389122a4a16e0dccb8cf556c08797a8c04
SrPepeR <kjrm97@gmail.com>
2023-12-28 21:05:22 +0000

    feat: Moved buttons styles to principal stylesheet

--- projects/ngx-monkey-ui/src/lib/components/button/button.component.ts
+++ projects/ngx-monkey-ui/src/lib/components/button/button.component.ts
@@ -4,13 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'monkey-button',
   templateUrl: './button.component.html',
   styleUrls: [
-    './button.component.scss',
-    './styles/button-default.component.scss',
-    './styles/button-brutalist.component.scss',
-    './styles/button-glassmorphism.component.scss',
-    './styles/button-flat.component.scss',
-    './styles/button-ghost.component.scss',
-    './styles/button-glow.component.scss',
+    './button.component.scss'
   ]
 })
 export class MonkeyButton {
