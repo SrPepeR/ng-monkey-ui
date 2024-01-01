14929086c5c65d202c3bc9d8107a3068cb34b815
SrPepeR <kjrm97@gmail.com>
2024-01-01 19:54:09 +0000

    feat: Added Style functionallity to Monkey Buttons

--- projects/ngx-monkey-ui-tests/src/app/app.component.ts
+++ projects/ngx-monkey-ui-tests/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  primaryStyle: Style = Style.primary;
+  secondaryStyle: Style = Style.secondary;
+  tertiaryStyle: Style = Style.tertiary;
+  warningStyle: Style = Style.warning;
+  dangerStyle: Style = Style.danger;
+  successStyle: Style = Style.success;
+  infoStyle: Style = Style.info;
+
   title = 'ngx-monkey-ui-tests';
+  currentStyle: Style = this.primaryStyle;
+
+  changeStyle(newStyle: Style): void {
+    this.currentStyle =newStyle;
+  }
+}
+
+enum Style {
+  primary = 'primary',
+  secondary = 'secondary',
+  tertiary = 'tertiary',
+
+  warning = 'warning',
+  danger = 'danger',
+  success = 'success',
+  info = 'info',
 }
