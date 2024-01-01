a6ea67662cec3fa1d840d4232b1a5ca755c7e097
SrPepeR <kjrm97@gmail.com>
2024-01-01 22:01:33 +0000

    feat: Created ComponentsStyle service for component type managment

---
+++ projects/ngx-monkey-ui/src/lib/services/components-styles.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComponentsStylesService {
+
+  constructor() { }
+
+  generateClassList(component: any) {
+    let classList: Array<string> = [];
+    let componentType = undefined;
+    
+    // Adds the component color scheme class to the classList array
+    if (component.style) {
+      classList.push(`style-${component.style}`);
+    }
+
+    if (component.brutalist === '') {
+      componentType = 'type-brutalist';
+    }
+
+    if (component.flat === '') {
+      componentType = 'type-flat';
+    }
+
+    if (component.ghost === '') {
+      componentType = 'type-ghost';
+    }
+
+    if (component.glass === '') {
+      componentType = 'type-glassmorphism';
+    }
+
+    if (component.glow === '') {
+      componentType = 'type-glow';
+    }
+
+    if (componentType) {
+      // Adds the component type class to the classList array if any type is defined
+      classList.push(componentType);
+    }
+
+    return classList;
+  }
+
+}
