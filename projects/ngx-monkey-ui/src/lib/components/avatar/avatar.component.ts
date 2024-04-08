805aacf8b7d4bff1611119a0b4c82dece1b4d82d
SrPepeR <kjrm97@gmail.com>
2024-04-08 21:55:43 +0100

    feat: Completed monkey avatar functionallity

--- projects/ngx-monkey-ui/src/lib/components/avatar/avatar.component.ts
+++ projects/ngx-monkey-ui/src/lib/components/avatar/avatar.component.ts
@@ -2,15 +2,16 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angu
 import { ThemeService } from '../../services/theme.service';
 import { ComponentsStylesService } from '../../services/components-styles.service';
 import { ComponentsSizesService } from '../../services/components-sizes.service';
+import { TooltipService } from '../../services/tooltip/tooltip.service';
 
 @Component({
   selector: 'monkey-avatar',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss']
 })
-export class MonkeyAvatar implements OnInit, OnChanges {
+export class MonkeyAvatar implements OnChanges {
 
-  @Input() style?: string = 'background';
+  @Input() style: string = 'primary';
 
   @Input() image: string = '';
   @Input() errorImage: string = '';
@@ -28,24 +29,35 @@ export class MonkeyAvatar implements OnInit, OnChanges {
   isDarkMode$ = this.themeService.isDarkMode$;
 
   classList: Array<string> = [];
-  
+
   size: { width: number, height: number } = { width: 0, height: 0 };
+  isLabeled: boolean = false;
 
   constructor(
     private themeService: ThemeService,
     private componentStylesService: ComponentsStylesService,
     private componentSizesService: ComponentsSizesService,
+    private tooltipService: TooltipService,
   ) { }
 
-  ngOnInit(): void {
-    this.checkSize();
+  ngOnChanges() {
+    this.checkAll();
   }
 
-  ngOnChanges() {
-    this.classList = this.componentStylesService.generateClassList(this);
-    this.classList = this.componentSizesService.generateClassList(this);
+  showTooltip(event: MouseEvent) {
+    if (this.isLabeled) {
+      return;
+    }
 
-    this.checkClasses();
+    this.tooltipService.show(this.text, this.style || 'primary', { x: event.pageX, y: event.pageY })
+  }
+
+  hideTooltip() {
+    if (this.isLabeled) {
+      return;
+    }
+
+    this.tooltipService.hide();
   }
 
   onClick() {
@@ -54,6 +66,27 @@ export class MonkeyAvatar implements OnInit, OnChanges {
 
   onErrorImage(): void {
     this.image = this.errorImage;
+  }
+
+  private checkAll() {
+    this.checkClasses();
+    this.checkSize();
+    this.checkLabeled();
+  }
+
+  private checkClasses() {
+    this.classList = this.componentStylesService.generateClassList(this);
+    this.classList = this.componentSizesService.generateClassList(this);
+
+    this.classList.push(`form-${this.form}`);
+
+    if (this.check(this.labeled)) {
+      this.classList.push(`labeled`);
+    }
+
+    if (this.form === 'rounded') {
+      this.classList.push('full-rounded');
+    }
   }
 
   private checkSize() {
@@ -66,16 +99,8 @@ export class MonkeyAvatar implements OnInit, OnChanges {
     }
   }
 
-  private checkClasses() {
-    this.classList.push(`form-${this.form}`);
-
-    if (this.check(this.labeled)) {
-      this.classList.push(`labeled`);
-    }
-
-    if (this.form === 'rounded') {
-      this.classList.push('full-rounded');
-    }
+  private checkLabeled() {
+    this.isLabeled = this.check(this.labeled);
   }
 
   private check(attribute?: string) {
