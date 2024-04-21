611c41f43644950ac7265c9d884c7954d5faa5a5
SrPepeR <kjrm97@gmail.com>
2024-04-21 18:03:11 +0100

    doc: Documented third level components

--- projects/ngx-monkey-ui/src/lib/components/third-level/alert/alert.component.ts
+++ projects/ngx-monkey-ui/src/lib/components/third-level/alert/alert.component.ts
@@ -5,6 +5,10 @@ import { MonkeyAlertService } from '../../../services/alert.service';
 import { Subject, takeUntil } from 'rxjs';
 import { Message } from './message';
 
+/**
+ * Represents the MonkeyAlert component.
+ * This component displays an alert message with optional dismiss, accept, and reject actions.
+ */
 @Component({
   selector: 'monkey-alert',
   templateUrl: './alert.component.html',
@@ -14,32 +18,94 @@ import { Message } from './message';
 })
 export class MonkeyAlert implements OnInit, OnChanges, OnDestroy {
 
+  /**
+   * Specifies whether the alert should be displayed in a column layout.
+   */
   @Input() column?: boolean = false;
 
+  /**
+   * The message to be displayed in the alert.
+   */
   @Input() message: Message = new Message([], 'warning', false);
 
+  /**
+   * The text to be displayed for the dismiss action.
+   */
   @Input() dismissibleText?: string = 'Dismiss';
+  
+  /**
+   * Event emitted when the dismiss action is triggered.
+   */
   @Output() onDismiss = new EventEmitter();
 
+  /**
+   * Specifies whether the alert can be accepted.
+   */
   @Input() acceptable?: string = 'true';
+  
+  /**
+   * The text to be displayed for the accept action.
+   */
   @Input() acceptText?: string = 'Accept';
+  
+  /**
+   * Event emitted when the accept action is triggered.
+   */
   @Output() onAccept = new EventEmitter();
 
+  /**
+   * Specifies whether the alert can be rejected.
+   */
   @Input() rejectable?: string = 'true';
+  
+  /**
+   * The text to be displayed for the reject action.
+   */
   @Input() rejectText?: string = 'Reject';
+  
+  /**
+   * Event emitted when the reject action is triggered.
+   */
   @Output() onReject = new EventEmitter();
 
-  // COMPONENTS TYPES
+  /**
+   * Specifies whether the alert should have brutalist style.
+   */
   @Input() brutalist?: string = 'true';
+  
+  /**
+   * Specifies whether the alert should have flat style.
+   */
   @Input() flat?: string = 'true';
+  
+  /**
+   * Specifies whether the alert should have ghost style.
+   */
   @Input() ghost?: string = 'true';
+  
+  /**
+   * Specifies whether the alert should have glass style.
+   */
   @Input() glass?: string = 'true';
+  
+  /**
+   * Specifies whether the alert should have glow style.
+   */
   @Input() glow?: string = 'true';
 
+  /**
+   * Observable that indicates whether the application is in dark mode.
+   */
   isDarkMode$ = this.themeService.isDarkMode$;
 
+  /**
+   * Array of CSS classes to be applied to the alert component.
+   */
   classList: Array<string> = [];
 
+  /**
+   * Subject used to unsubscribe from observables when the component is destroyed.
+   */
   private unsubscribeComponent: Subject<void> = new Subject<void>();
 
   constructor(
@@ -48,6 +114,11 @@ export class MonkeyAlert implements OnInit, OnChanges, OnDestroy {
     private alertService: MonkeyAlertService,
   ) { }
 
+  /**
+   * Initializes the component.
+   * Subscribes to the alertService event to receive alert messages.
+   * Hides the alert initially.
+   */
   ngOnInit() {
     this.alertService.event
       .pipe(takeUntil(this.unsubscribeComponent))
@@ -58,34 +129,58 @@ export class MonkeyAlert implements OnInit, OnChanges, OnDestroy {
     this.alertService.hide();
   }
 
+  /**
+   * Called when the input properties of the component change.
+   * Generates the class list based on the component's input properties.
+   */
   ngOnChanges() {
     this.classList = this.componentStylesService.generateClassList(this);
   }
 
+  /**
+   * Cleans up resources when the component is destroyed.
+   */
   ngOnDestroy() {
     this.unsubscribeComponent.next();
     this.unsubscribeComponent.complete();
   }
 
+  /**
+   * Dismisses the alert and emits the onDismiss event.
+   */
   dismissSelected() {
     this.dismissAlert();
     this.onDismiss.emit();
   }
 
+  /**
+   * Emits the onReject event and dismisses the alert.
+   */
   rejectSelected() {
     this.onReject.emit();
     this.dismissAlert();
   }
 
+  /**
+   * Emits the onAccept event and dismisses the alert.
+   */
   acceptSelected() {
     this.onAccept.emit();
     this.dismissAlert();
   }
 
+  /**
+   * Hides the alert using the alertService.
+   */
   private dismissAlert() {
     this.alertService.hide();
   }
 
+  /**
+   * Checks if a parameter is not equal to 'true'.
+   * @param param - The parameter to check.
+   * @returns True if the parameter is not equal to 'true', false otherwise.
+   */
   check(param: string | undefined) {
     return param !== 'true';
   }
--- projects/ngx-monkey-ui/src/lib/components/third-level/alert/message.ts
+++ projects/ngx-monkey-ui/src/lib/components/third-level/alert/message.ts
@@ -1,19 +1,51 @@
+/**
+ * Represents a message to be displayed in an alert.
+ */
 export class Message {
-
+    /**
+     * The texts to be displayed in the message.
+     */
     texts: string[];
+
+    /**
+     * The style of the message.
+     */
     style: string;
+
+    /**
+     * Indicates whether the message should automatically close.
+     */
     isAutoClose: boolean = true;
 
+    /**
+     * The title of the message.
+     */
     title?: string;
+
+    /**
+     * The icon to be displayed with the message.
+     */
     icon?: string;
 
-    constructor(texts: string[], style: string, isAutoClose: boolean, title?: string, icon?: string) {
+    /**
+     * Creates a new instance of the Message class.
+     * @param texts The texts to be displayed in the message.
+     * @param style The style of the message.
+     * @param isAutoClose Indicates whether the message should automatically close.
+     * @param title The title of the message.
+     * @param icon The icon to be displayed with the message.
+     */
+    constructor(
+        texts: string[],
+        style: string,
+        isAutoClose: boolean,
+        title?: string,
+        icon?: string
+    ) {
         this.texts = texts;
         this.style = style;
         this.isAutoClose = isAutoClose;
-
         this.title = title;
         this.icon = icon;
     }
-
 }
