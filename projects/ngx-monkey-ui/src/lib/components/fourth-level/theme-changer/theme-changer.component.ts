8cc334f5621c01f07110adc0e1cd1bb68385e06d
SrPepeR <kjrm97@gmail.com>
2024-04-21 18:03:53 +0100

    doc: Documented fourth level components

--- projects/ngx-monkey-ui/src/lib/components/fourth-level/theme-changer/theme-changer.component.ts
+++ projects/ngx-monkey-ui/src/lib/components/fourth-level/theme-changer/theme-changer.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { ThemeService } from '../../../services/theme.service';
 
+/**
+ * Component for changing the theme of the application.
+ */
 @Component({
   selector: 'monkey-theme-changer',
   templateUrl: './theme-changer.component.html',
@@ -8,14 +11,24 @@ import { ThemeService } from '../../../services/theme.service';
 })
 export class MonkeyThemeChanger {
 
+  /**
+   * Event emitter for when an action is performed.
+   */
   @Output() onAction = new EventEmitter();
 
+  /**
+   * Observable that indicates whether the dark mode is enabled or not.
+   */
   isDarkMode$ = this.themeService.isDarkMode$;
 
   constructor(
     private themeService: ThemeService,
   ) { }
 
+  /**
+   * Toggles the theme between light and dark mode.
+   * Emits the `onAction` event.
+   */
   toggleTheme() {
     this.themeService.toggleDarkMode();
     this.onAction.emit();
