ad31ba7ec2b1a795dfca9d26cc89302dd34b9c44
SrPepeR <kjrm97@gmail.com>
2024-04-21 17:23:11 +0100

    doc: Documented card component

--- projects/ngx-monkey-ui/src/lib/components/first-level/card/card.component.ts
+++ projects/ngx-monkey-ui/src/lib/components/first-level/card/card.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { ThemeService } from '../../../services/theme.service';
 import { ComponentsStylesService } from '../../../services/components-styles.service';
 
+/**
+ * Represents a Monkey Card component.
+ */
 @Component({
   selector: 'monkey-card',
   templateUrl: './card.component.html',
@@ -16,22 +19,66 @@ import { ComponentsStylesService } from '../../../services/components-styles.ser
 })
 export class MonkeyCard implements OnInit, OnChanges {
   
+  /**
+   * Specifies whether the card should be displayed in a column layout.
+   */
   @Input() column?: boolean = false;
+  
+  /**
+   * Specifies the style of the card.
+   */
   @Input() style?: string = 'background';
   
   // COMPONENTS TYPES
+  
+  /**
+   * Specifies whether the brutalist style should be applied to the card.
+   */
   @Input() brutalist?: string = 'true';
+  
+  /**
+   * Specifies whether the flat style should be applied to the card.
+   */
   @Input() flat?: string = 'true';
+  
+  /**
+   * Specifies whether the ghost style should be applied to the card.
+   */
   @Input() ghost?: string = 'true';
+  
+  /**
+   * Specifies whether the glass style should be applied to the card.
+   */
   @Input() glass?: string = 'true';
+  
+  /**
+   * Specifies whether the glow style should be applied to the card.
+   */
   @Input() glow?: string = 'true';
 
+  /**
+   * Specifies whether padding should be removed from the card.
+   */
   @Input() noPadding?: string = 'true';
+  
+  /**
+   * Specifies whether margin should be removed from the card.
+   */
   @Input() noMargin?: string = 'true';
+  
+  /**
+   * Specifies whether the flex-wrap should be reversed for the card.
+   */
   @Input() displayFlexWrapReverse?: string = 'true';
 
+  /**
+   * Observable that emits the current dark mode state.
+   */
   isDarkMode$ = this.themeService.isDarkMode$;
 
+  /**
+   * Array of CSS class names to be applied to the card.
+   */
   classList: Array<string> = [];
 
   constructor(
@@ -39,10 +86,16 @@ export class MonkeyCard implements OnInit, OnChanges {
     private componentStylesService: ComponentsStylesService,
   ) { }
 
+  /**
+   * Initializes the component.
+   */
   ngOnInit() {
     this.classList = this.componentStylesService.generateClassList(this);
   }
 
+  /**
+   * Called whenever the input properties change.
+   */
   ngOnChanges() {
     this.classList = this.componentStylesService.generateClassList(this);
   }
