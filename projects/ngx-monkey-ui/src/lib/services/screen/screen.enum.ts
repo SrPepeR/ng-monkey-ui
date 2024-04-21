dad47dabe0a0912e2ec7f0810bc1faacd07a01d5
SrPepeR <kjrm97@gmail.com>
2024-04-21 15:22:06 +0100

    feat: Added screen service and enum for managing screen-related functionality

---
+++ projects/ngx-monkey-ui/src/lib/services/screen/screen.enum.ts
@@ -0,0 +1,28 @@
+export enum ScreenSize {
+
+  XS = 600,
+  SM = 768,
+  MD = 992,
+  LG = 1200,
+  XL = 1400,
+  XXL = 1600
+
+}
+
+export enum ScreenSizeStyleClass {
+
+  XS = 'screen-xs',
+  SM = 'screen-sm',
+  MD = 'screen-md',
+  LG = 'screen-lg',
+  XL = 'screen-xl',
+  XXL = 'screen-xxl'
+
+}
+
+export enum ScreenOrientation {
+
+  PORTRAIT = 'portrait',
+  LANDSCAPE = 'landscape',
+
+}
