26ae41b5cd27131c0861c25e925c6db6f3da29c3
SrPepeR <kjrm97@gmail.com>
2024-04-21 18:03:11 +0100

    doc: Documented third level components

--- projects/ngx-monkey-ui/src/lib/components/third-level/menu/menu.component.ts
+++ projects/ngx-monkey-ui/src/lib/components/third-level/menu/menu.component.ts
@@ -7,6 +7,9 @@ import { MenuOption } from './menu-option.interface';
 import { Router } from '@angular/router';
 import { MonkeyScreen } from '../../../services/screen/screen';
 
+/**
+ * Represents a menu component that displays a list of options.
+ */
 @Component({
   selector: 'monkey-menu',
   templateUrl: './menu.component.html',
@@ -17,17 +20,46 @@ import { MonkeyScreen } from '../../../services/screen/screen';
 })
 export class MonkeyMenu extends Styleable {
 
+  /**
+   * Represents the tooltipable behavior of the menu component.
+   */
   tooltipable: Tooltipable;
 
+  /**
+   * The title of the menu.
+   */
   @Input() title?: string;
+
+  /**
+   * The list of menu options.
+   */
   @Input() options!: MenuOption[];
+
+  /**
+   * Determines whether the menu should navigate to the selected option's route.
+   * Defaults to 'false'.
+   */
   @Input() selfNavigation?: string = 'false';
 
+  /**
+   * Event emitted when a menu option is selected.
+   */
   @Output() optionSelected: EventEmitter<MenuOption> = new EventEmitter();
 
+  /**
+   * Observable that indicates whether the dark mode is enabled.
+   */
   isDarkMode$ = this.themeService.isDarkMode$;
 
+  /**
+   * The alternative text for the menu.
+   */
   alt!: string;
+
+  /**
+   * Determines whether the full menu is open or not.
+   * Defaults to true.
+   */
   isFullMenuOpen: boolean = true;
 
   constructor(
@@ -39,12 +71,18 @@ export class MonkeyMenu extends Styleable {
     this.tooltipable = new Tooltipable(this.tooltipService);
   }
 
+  /**
+   * Initializes the component.
+   */
   override ngOnInit() {
     super.ngOnInit();
     this.tooltipable.alt = this.alt;
     this.tooltipable.style = this.style;
   }
 
+  /**
+   * Handles changes to the component's input properties.
+   */
   override ngOnChanges() {
     super.ngOnChanges();
     this.tooltipable.alt = this.alt;
@@ -59,16 +97,27 @@ export class MonkeyMenu extends Styleable {
     });
   }
 
+  /**
+   * Handles the selection of a menu option.
+   * @param option The selected menu option.
+   */
   onSelectedChanged(option: MenuOption) {
     if (option.route) {
       this.navigateToPage(option);
     }
   }
 
+  /**
+   * Navigates to the root page.
+   */
   navigateToRootPage() {
     this.navigateToPage({ label: this.title || '', route: '/' });
   }
 
+  /**
+   * Navigates to the specified menu option's route.
+   * @param menuOption The menu option to navigate to.
+   */
   navigateToPage(menuOption: MenuOption) {
     this.optionSelected.emit(menuOption);
     
@@ -77,6 +126,9 @@ export class MonkeyMenu extends Styleable {
     }
   }
 
+  /**
+   * Toggles the full menu open or closed.
+   */
   toggleFullMenu() {
     this.isFullMenuOpen = !this.isFullMenuOpen;
   }
--- projects/ngx-monkey-ui/src/lib/components/third-level/tooltip/tooltip.component.ts
+++ projects/ngx-monkey-ui/src/lib/components/third-level/tooltip/tooltip.component.ts
@@ -5,6 +5,9 @@ import { Tooltip } from '../../../services/tooltip/tooltip';
 import { Subject, takeUntil } from 'rxjs';
 import { TooltipService } from '../../../services/tooltip/tooltip.service';
 
+/**
+ * Represents the MonkeyTooltip component.
+ */
 @Component({
   selector: 'monkey-tooltip',
   templateUrl: './tooltip.component.html',
@@ -14,21 +17,51 @@ import { TooltipService } from '../../../services/tooltip/tooltip.service';
 })
 export class MonkeyTooltip implements OnInit, OnChanges, OnDestroy {
   
+  /**
+   * The tooltip input property.
+   */
   @Input() tooltip: Tooltip = new Tooltip('', '', undefined);
   
   // COMPONENTS TYPES
+  /**
+   * The brutalist input property.
+   */
   @Input() brutalist?: string = 'true';
+  /**
+   * The flat input property.
+   */
   @Input() flat?: string = 'true';
+  /**
+   * The ghost input property.
+   */
   @Input() ghost?: string = 'true';
+  /**
+   * The glass input property.
+   */
   @Input() glass?: string = 'true';
+  /**
+   * The glow input property.
+   */
   @Input() glow?: string = 'true';
 
+  /**
+   * The onClick output event.
+   */
   @Output() onClick = new EventEmitter();
 
+  /**
+   * Observable that indicates whether the dark mode is enabled or not.
+   */
   isDarkMode$ = this.themeService.isDarkMode$;
 
+  /**
+   * Array of CSS class names.
+   */
   classList: Array<string> = [];
 
+  /**
+   * Subject used to unsubscribe from observables.
+   */
   private unsubscribeComponent: Subject<void> = new Subject<void>();
 
   constructor(
@@ -37,6 +70,9 @@ export class MonkeyTooltip implements OnInit, OnChanges, OnDestroy {
     private tooltipService: TooltipService,
   ) { }
 
+  /**
+   * Lifecycle hook that is called after the component has been initialized.
+   */
   ngOnInit() {
     this.tooltipService.event
       .pipe(takeUntil(this.unsubscribeComponent))
@@ -47,10 +83,16 @@ export class MonkeyTooltip implements OnInit, OnChanges, OnDestroy {
     this.tooltipService.hide();
   }
 
+  /**
+   * Lifecycle hook that is called when any input property changes.
+   */
   ngOnChanges() {
     this.classList = this.componentStylesService.generateClassList(this);
   }
 
+  /**
+   * Lifecycle hook that is called when the component is about to be destroyed.
+   */
   ngOnDestroy() {
     this.unsubscribeComponent.next();
     this.unsubscribeComponent.complete();
