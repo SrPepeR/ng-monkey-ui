64ae936a4250dd5df36fb202809973811f1f6a76
SrPepeR <kjrm97@gmail.com>
2024-04-27 14:55:24 +0100

    feat: Created components portfolio page in tests

---
+++ projects/ngx-monkey-ui-tests/src/app/pages/components-portfolio/components-portfolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components-portfolio',
+  templateUrl: './components-portfolio.component.html',
+  styleUrls: ['./components-portfolio.component.scss']
+})
+export class ComponentsPortfolioComponent {
+
+}
