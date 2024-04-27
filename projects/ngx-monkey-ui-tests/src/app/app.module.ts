fdfb9a124c9d7eb077d8c540a07ee6ac743ddb5a
SrPepeR <kjrm97@gmail.com>
2024-04-27 16:24:29 +0100

    feat: Added new routes to app-routing

--- projects/ngx-monkey-ui-tests/src/app/app.module.ts
+++ projects/ngx-monkey-ui-tests/src/app/app.module.ts
@@ -5,23 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgxMonkeyUiModule } from 'ngx-monkey-ui';
-import { ExamplePageComponent } from './pages/example-page/example-page.component';
-import { ComponentsPortfolioComponent } from './pages/components-portfolio/components-portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    // Pages
-    ExamplePageComponent,
-    ComponentsPortfolioComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgxMonkeyUiModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
