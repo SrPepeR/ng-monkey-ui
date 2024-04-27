a95b046a08f4988dfc499da5b5d123da39cd4064
SrPepeR <kjrm97@gmail.com>
2024-04-27 23:45:30 +0100

    Refactor components-portfolio.component.ts to add dropdown options and component types

--- projects/ngx-monkey-ui-tests/src/app/pages/components-portfolio/components-portfolio.component.ts
+++ projects/ngx-monkey-ui-tests/src/app/pages/components-portfolio/components-portfolio.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MonkeyAlertService, MonkeyFontService, MonkeyStyle } from 'ngx-monkey-ui';
+import { DropdownOption, MonkeyAlertService, MonkeyFontService, MonkeyStyle } from 'ngx-monkey-ui';
 import { MonkeyButtonData } from 'projects/ngx-monkey-ui/src/lib/objects/classes/button-data.class';
 import { map } from 'rxjs';
 
@@ -10,17 +10,31 @@ import { map } from 'rxjs';
   styleUrls: ['./components-portfolio.component.scss']
 })
 export class ComponentsPortfolioComponent {
-  
+
+  componentsTypes: MonkeyButtonData[] = [
+    new MonkeyButtonData(MonkeyStyle.PRIMARY, 'Default', () => this.setType('Default'), 'star', 'left'),
+    new MonkeyButtonData(MonkeyStyle.SECONDARY, 'Brutalist', () => this.setType('Brutalist'), 'gavel', 'left'),
+    new MonkeyButtonData(MonkeyStyle.TERTIARY, 'Glass', () => this.setType('Glass'), 'wine_bar', 'left'),
+    new MonkeyButtonData(MonkeyStyle.TERTIARY, 'Flat', () => this.setType('Flat'), 'tools_flat_head', 'right'),
+    new MonkeyButtonData(MonkeyStyle.SECONDARY, 'Ghost', () => this.setType('Ghost'), 'partly_cloudy_night', 'right'),
+    new MonkeyButtonData(MonkeyStyle.PRIMARY, 'Glow', () => this.setType('Glow'), 'stylus_laser_pointer', 'right'),
+  ];
+
+  dropdownOptions: DropdownOption[] = [
+    { label: 'Success alert', icon: 'done', value: 'success' },
+    { label: 'Warning alert', icon: 'warning', value: 'warning' },
+    { label: 'Danger alert', icon: 'dangerous', value: 'danger' },
+    { label: 'Info alert', icon: 'info', value: 'info' },
+  ];
+
+  currentType: string = 'Default';
+
   currentStyle: MonkeyStyle = MonkeyStyle.PRIMARY;
 
   offSwitchText: string = 'Off';
   onSwitchText: string = 'On';
 
-  buttonsGroupData: MonkeyButtonData[] = [
-    new MonkeyButtonData(MonkeyStyle.PRIMARY, 'First', () => this.onClicked('First'), 'info', 'left'),
-    new MonkeyButtonData(MonkeyStyle.SECONDARY, 'Second', () => this.onClicked('Second')),
-    new MonkeyButtonData(MonkeyStyle.TERTIARY, 'Third', () => this.onClicked('Third'), 'info', 'right'),
-  ];
+  contentHeaderAction: MonkeyButtonData = new MonkeyButtonData(this.currentStyle, 'Show alert', () => this.showAlert('Content header'), 'info', 'right');
 
   constructor(
     private alertService: MonkeyAlertService,
@@ -64,6 +78,10 @@ export class ComponentsPortfolioComponent {
     }
   }
 
+  setType(newType: string): void {
+    this.currentType = newType;
+  }
+
   changeStyle(newStyle: MonkeyStyle): void {
     this.currentStyle = newStyle;
   }
@@ -78,6 +96,30 @@ export class ComponentsPortfolioComponent {
 
   onCheck(fromCheckbox: string, checked: Boolean): void {
     this.alertService.successes(['CheckBox ' + fromCheckbox + '.', checked ? 'ACTIVADO' : 'DESACTIVADO'], true, 'Success');
+  }
+
+  showAlert(text: string): void {
+    this.alertService.successes([`${text}`], true, 'Success');
+  }
+
+  onSelectedChanged(DropdownOption: DropdownOption): void {
+    switch (DropdownOption.value) {
+      case 'success':
+        this.alertService.successes(['Selected option: ' + DropdownOption.label], true, 'Success');
+        break;
+      case 'warning':
+        this.alertService.warnings(['Selected option: ' + DropdownOption.label], true, 'Warning');
+        break;
+      case 'danger':
+        this.alertService.dangers(['Selected option: ' + DropdownOption.label], true, 'Danger');
+        break;
+      case 'info':
+        this.alertService.infos(['Selected option: ' + DropdownOption.label], true, 'Info');
+        break;
+      default:
+        this.alertService.customs(['Selected option: ' + DropdownOption.label], MonkeyStyle.PRIMARY, true, 'Primary', 'looks_one');
+        break;
+    }
   }
 
 }
