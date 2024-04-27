d45d06a82b73972086a22890f37e266876f36a3e
SrPepeR <kjrm97@gmail.com>
2024-04-27 16:24:29 +0100

    feat: Added new routes to app-routing

--- projects/ngx-monkey-ui-tests/src/app/app-routing.module.ts
+++ projects/ngx-monkey-ui-tests/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'components',
+    loadChildren: () => import('./pages/components-portfolio/components-portfolio.module').then(m => m.ComponentsPortfolioModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/example-page/example-page.module').then(m => m.ExamplePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
